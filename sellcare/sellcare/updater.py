b9a459c818b2553fd7c794f2655c74a2013ad87e
lasalesi <larsericmueller@gmail.com>
2020-01-23 10:45:15 +0100

    Update updater.py
    Include german

--- sellcare/sellcare/updater.py
+++ sellcare/sellcare/updater.py
@@ -7,6 +7,6 @@ from frappe import _
 def cleanup_languages():
     # this function will remove languages after migrate
     print("Removing unnecessary languages...")
-    sql_query = """DELETE FROM `tabLanguage` WHERE `language_code` NOT IN ('it', 'fr', 'en-US', 'en', 'fr');"""
+    sql_query = """DELETE FROM `tabLanguage` WHERE `language_code` NOT IN ('it', 'de', 'en-US', 'en', 'fr');"""
     frappe.db.sql(sql_query, as_dict=1)
     return
