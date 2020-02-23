beb368bb95c2514bfc2d2e5a5bab22d3e2ec50b3
lasalesi <larsericmueller@gmail.com>
2020-02-23 20:47:38 +0100

    updated menu and transaltions

--- sellcare/config/sellcare.py
+++ sellcare/config/sellcare.py
@@ -4,7 +4,7 @@ from frappe import _
 def get_data():
     return[
         {
-            "label": _("CRM"),
+            "label": _("Kunden Auftragsdokumente"),
             "icon": "octicon octicon-organization",
             "items": [
                    {
@@ -15,51 +15,140 @@ def get_data():
                    },
                    {
                        "type": "doctype",
+                       "name": "Quotation",
+                       "label": _("Quotation"),
+                       "description": _("Quotation")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Angebotene Preise pro Kunde",
+                        "label": _("Offerte & Preis"),
+                        "doctype": "Quotation",
+                        "is_query_report": False
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Sales Order",
+                       "label": _("Kunden-AB Übersicht"),
+                       "description": _("Kunden-AB Übersicht")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Timetable AB (SQL)",
+                        "label": _("Timetable AB"),
+                        "doctype": "Sales Order",
+                        "is_query_report": True
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Delivery Note",
+                       "label": _("Kunden-LS Übersicht"),
+                       "description": _("Kunden-LS Übersicht")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Timetable LS (SQL)",
+                        "label": _("Timetable LS"),
+                        "doctype": "Delivery Note",
+                        "is_query_report": True
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Sales Invoice",
+                       "label": _("Kunden-RG Übersicht"),
+                       "description": _("Kunden-RG Übersicht")
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Blanket Order",
+                       "label": _("Rahmenauftrag"),
+                       "description": _("Rahmenauftrag")
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Journal Entry",
+                       "label": _("Jahresrückvergütung & Gutschrift"),
+                       "description": _("Jahresrückvergütung & Gutschrift")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Mahnung",
+                        "label": _("Mahnung"),
+                        "doctype": "Payment Reminder",
+                        "is_query_report": False
+                   }
+            ]
+        },
+        {
+            "label": _("Beschaffung & Lager"),
+            "icon": "octicon octicon-organization",
+            "items": [
+                   {
+                        "type": "doctype",
+                        "name": "Item",
+                        "label": _("Item"),
+                        "description": _("Item")
+                   },
+                   {
+                        "type": "doctype",
+                        "name": "Supplier",
+                        "label": _("Supplier"),
+                        "description": _("Supplier")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Lieferantenbestellung (SQL)",
+                        "label": "Lieferanten-AB", 
+                        "doctype": "Purchase Order",
+                        "is_query_report": True
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Purchase Invoice",
+                       "label": _("Lieferanten-RG"),
+                       "description": _("Lieferanten-RG")
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Purchase Receipt",
+                       "label": _("Wareneingang"),
+                       "description": _("Wareneingang")
+                   },
+                   {
+                        "type": "report",
+                        "name": "Order Planning",
+                        "doctype": "Item",
+                        "is_query_report": True
+                   }
+            ]
+        },
+        {
+            "label": _("CRM"),
+            "icon": "octicon octicon-organization",
+            "items": [
+                   {
+                       "type": "doctype",
+                       "name": "Contact",
+                       "label": _("Kontaktperson"),
+                       "description": _("Kontaktperson")
+                   },
+                   {
+                       "type": "doctype",
+                       "name": "Customer",
+                       "label": _("Customer"),
+                       "description": _("Customer")
+                   },
+                   {
+                       "type": "doctype",
                        "name": "Customer Visit Report",
-                       "label": _("Customer Visit Report"),
-                       "description": _("Customer Visit Report")
+                       "label": _("Übersicht Besuchsberichte"),
+                       "description": _("Übersicht Besuchsberichte")
                    },
                    {
                        "type": "doctype",
                        "name": "Customer Item Text",
                        "label": _("Customer Item Text"),
                        "description": _("Customer Item Text")
-                   }
-            ]
-        },
-        {
-            "label": _("Procurement"),
-            "icon": "octicon octicon-organization",
-            "items": [
-                   {
-                        "type": "report",
-                        "name": "Order Planning",
-                        "doctype": "Item",
-                        "is_query_report": True
-                   },
-                   {
-                       "type": "doctype",
-                       "name": "Purchase Order",
-                       "label": _("Purchase Order"),
-                       "description": _("Purchase Order")
-                   },
-                   {
-                       "type": "doctype",
-                       "name": "Sales Order",
-                       "label": _("Sales Order"),
-                       "description": _("Sales Order")
-                   },
-                   {
-                        "type": "report",
-                        "name": "Dispo-Report",
-                        "doctype": "Sales Order",
-                        "is_query_report": True
-                   },
-                   {
-                        "type": "report",
-                        "name": "Auswertung DB1",
-                        "doctype": "Sales Invoice",
-                        "is_query_report": True
                    }
             ]
         },
