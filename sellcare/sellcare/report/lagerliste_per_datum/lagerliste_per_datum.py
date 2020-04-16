312e7938b2a7221e8294ecb596d178a1c1d57825
lasalesi <larsericmueller@gmail.com>
2020-04-16 17:31:01 +0200

    Update lagerliste_per_datum.py
    Dynamic date range

--- sellcare/sellcare/report/lagerliste_per_datum/lagerliste_per_datum.py
+++ sellcare/sellcare/report/lagerliste_per_datum/lagerliste_per_datum.py
@@ -35,13 +35,13 @@ def get_data(filters):
                         IFNULL(SUM(`sle1`.`stock_value_difference`), 0)
                      FROM `tabStock Ledger Entry` AS `sle1`
                      WHERE `sle1`.`item_code` = `tabItem`.`item_code`
-                           AND `sle1`.`posting_date` <= '2019-12-31'
+                           AND `sle1`.`posting_date` <= '{date}'
                            AND `sle1`.`warehouse` LIKE '{warehouse}') AS `value`,
                     (SELECT
                         IFNULL(SUM(`sle2`.`actual_qty`), 0)
                      FROM `tabStock Ledger Entry` AS `sle2`
                      WHERE `sle2`.`item_code` = `tabItem`.`item_code`
-                           AND `sle2`.`posting_date` <= '2019-12-31'
+                           AND `sle2`.`posting_date` <= '{date}'
                            AND `sle2`.`warehouse` LIKE '{warehouse}') AS `qty`
                     FROM `tabItem`
                     WHERE `is_stock_item` = 1;""".format(date=filters.date, warehouse=filters.warehouse)
